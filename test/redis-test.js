961b18047acf93a0e4d585fc5502c7f4b4d6693e
Karl Svartholm <gorbiz@gmail.com>
2015-05-08 00:26:57 +0200

    ping() gives 'PONG' & better test desc

--- test/redis-test.js
+++ test/redis-test.js
@@ -3,18 +3,24 @@ var expect = require("chai").expect
   , redis = require("../lib").Redis;
 
 describe("Redis", function() {
-  describe("#set", function() {
 
-    it("accepts 2 params", function() {
+  describe("#ping", function() {
+    it("pongs", function() {
+      expect(redis.ping()).to.equal("PONG");
+    });
+  });
+
+  describe("#set", function() {
+    it("takes 2 params", function() {
       expect(redis.set("server:name", "fido")).to.be.ok;
     });
 
-    it("stores a VALUE in a KEY", function() {
+    it("a value set is gettable", function() {
       redis.set("server:name", "fido");
       expect(redis.get("server:name")).to.equal("fido");
     });
 
-    it("stores multiple KEY - VALUE pairs", function() {
+    it("multiple values set are gettable", function() {
       redis.set("server:name", "fido");
       redis.set("server:admin", "slaygon");
       expect(redis.get("server:name")).to.equal("fido");
