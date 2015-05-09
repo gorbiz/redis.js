20e9fde584f693df82f24d0925923c0bd8fdbb7d
Karl Svartholm <gorbiz@gmail.com>
2015-05-09 16:57:49 +0200

    :art: s/"/'/g
    some atom snippets package use single quotes, so why not

--- test/redis.js
+++ test/redis.js
@@ -1,52 +1,52 @@
-var expect = require("chai").expect
-  , should = require("chai").should
-  , redis = require("../lib").Redis();
+var expect = require('chai').expect
+  , should = require('chai').should
+  , redis = require('../lib').Redis();
 
-describe("Redis", function() {
+describe('Redis', function() {
 
-  describe("#ping", function() {
-    it("pongs", function() {
-      expect(redis.ping()).to.equal("PONG");
+  describe('#ping', function() {
+    it('pongs', function() {
+      expect(redis.ping()).to.equal('PONG');
     });
   });
 
-  describe("#set", function() {
-    it("takes 2 params", function() {
-      expect(redis.set("server:name", "fido")).to.be.ok;
+  describe('#set', function() {
+    it('takes 2 params', function() {
+      expect(redis.set('server:name', 'fido')).to.be.ok;
     });
 
-    it("has more tests?");
+    it('has more tests?');
   });
 
-  describe("#get", function() {
-    it("a value set is gettable", function() {
-      redis.set("server:name", "fido");
-      expect(redis.get("server:name")).to.equal("fido");
+  describe('#get', function() {
+    it('a value set is gettable', function() {
+      redis.set('server:name', 'fido');
+      expect(redis.get('server:name')).to.equal('fido');
     });
 
-    it("multiple values set are gettable", function() {
-      redis.set("server:name", "fido");
-      redis.set("server:admin", "slaygon");
-      expect(redis.get("server:name")).to.equal("fido");
-      expect(redis.get("server:admin")).to.equal("slaygon");
+    it('multiple values set are gettable', function() {
+      redis.set('server:name', 'fido');
+      redis.set('server:admin', 'slaygon');
+      expect(redis.get('server:name')).to.equal('fido');
+      expect(redis.get('server:admin')).to.equal('slaygon');
     });
 
-    it("undefined keys are undefined", function() {
-      expect(redis.get("not-defined")).to.be.undefined;
+    it('undefined keys are undefined', function() {
+      expect(redis.get('not-defined')).to.be.undefined;
     });
   });
 
-  describe("#incr", function() {
-    it("increments a number by 1");
+  describe('#incr', function() {
+    it('increments a number by 1');
   });
 
-  describe("#ttl", function() {
-    it("handels keys with no EXPIRE set");
-    it("GET past EXPIRE not possible");
+  describe('#ttl', function() {
+    it('handels keys with no EXPIRE set');
+    it('GET past EXPIRE not possible');
   });
 
-  describe("errors", function() {
-    it("SET friends Linus & RPUSH friends Pelle errors");
+  describe('errors', function() {
+    it('SET friends Linus & RPUSH friends Pelle errors');
     // (error) WRONGTYPE Operation against a key holding the wrong kind of value
   });
 });
--- test/try-redis-io.js
+++ test/try-redis-io.js
@@ -1,57 +1,57 @@
-var expect = require("chai").expect
-  , should = require("chai").should
-  , redis = require("../lib").Redis();
+var expect = require('chai').expect
+  , should = require('chai').should
+  , redis = require('../lib').Redis();
 
-describe("try.redis.io", function() {
-  describe("parts", function() {
-    it("1 - GET & SET", function() {
-      redis.set("server:name", "fido");
-      expect(redis.get("server:name")).to.equal("fido");
+describe('try.redis.io', function() {
+  describe('parts', function() {
+    it('1 - GET & SET', function() {
+      redis.set('server:name', 'fido');
+      expect(redis.get('server:name')).to.equal('fido');
     });
 
-    it("2 - INCR & DEL", function() {
-      redis.set("connections", 10);
-      expect(redis.incr("connections")).to.equal(11);
-      expect(redis.incr("connections")).to.equal(12);
-      redis.del("connections");
-      expect(redis.incr("connections")).to.equal(1);
+    it('2 - INCR & DEL', function() {
+      redis.set('connections', 10);
+      expect(redis.incr('connections')).to.equal(11);
+      expect(redis.incr('connections')).to.equal(12);
+      redis.del('connections');
+      expect(redis.incr('connections')).to.equal(1);
     });
 
-    it("3 - Atomic INCR");
+    it('3 - Atomic INCR');
 
-    it("4 - EXPIRE & TTL", function() {
-      redis.set("resource:lock", "Redis Demo");
-      redis.expire("resource:lock", 120);
+    it('4 - EXPIRE & TTL', function() {
+      redis.set('resource:lock', 'Redis Demo');
+      redis.expire('resource:lock', 120);
       redis.debugTimetravel(7);
-      expect(redis.ttl("resource:lock")).to.equal(113);
+      expect(redis.ttl('resource:lock')).to.equal(113);
       redis.debugTimetravel(113);
-      expect(redis.ttl("resource:lock")).to.equal(-2);
+      expect(redis.ttl('resource:lock')).to.equal(-2);
 
-      redis.set("resource:lock", "Redis Demo 1");
-      redis.expire("resource:lock", 120);
+      redis.set('resource:lock', 'Redis Demo 1');
+      redis.expire('resource:lock', 120);
       redis.debugTimetravel(1);
-      expect(redis.ttl("resource:lock")).to.equal(119);
-      redis.set("resource:lock", "Redis Demo 2");
-      expect(redis.ttl("resource:lock")).to.equal(-1);
+      expect(redis.ttl('resource:lock')).to.equal(119);
+      redis.set('resource:lock', 'Redis Demo 2');
+      expect(redis.ttl('resource:lock')).to.equal(-1);
     });
 
-    it("5 - RPUSH, LPUSH & LRANGE", function() {
-      redis.rpush("friends", "Alice");
-      redis.rpush("friends", "Bob");
-      redis.lpush("friends", "Sam");
+    it('5 - RPUSH, LPUSH & LRANGE', function() {
+      redis.rpush('friends', 'Alice');
+      redis.rpush('friends', 'Bob');
+      redis.lpush('friends', 'Sam');
 
-      expect(redis.lrange("friends", 0, -1)).to.deep.equal(["Sam", "Alice", "Bob"]);
-      expect(redis.lrange("friends", 0, 1)).to.deep.equal(["Sam", "Alice"]);
-      expect(redis.lrange("friends", 1, 2)).to.deep.equal(["Alice", "Bob"]);
+      expect(redis.lrange('friends', 0, -1)).to.deep.equal(['Sam', 'Alice', 'Bob']);
+      expect(redis.lrange('friends', 0, 1)).to.deep.equal(['Sam', 'Alice']);
+      expect(redis.lrange('friends', 1, 2)).to.deep.equal(['Alice', 'Bob']);
     });
 
-    it("6 - LLEN, LPOP & RPOP", function() {
-      expect(redis.llen("friends")).to.equal(3);
-      expect(redis.lpop("friends")).to.equal("Sam");
-      expect(redis.rpop("friends")).to.equal("Bob");
+    it('6 - LLEN, LPOP & RPOP', function() {
+      expect(redis.llen('friends')).to.equal(3);
+      expect(redis.lpop('friends')).to.equal('Sam');
+      expect(redis.rpop('friends')).to.equal('Bob');
 
-      expect(redis.llen("friends")).to.equal(1);
-      expect(redis.lrange("friends", 0, -1)).to.deep.equal(["Alice"]);
+      expect(redis.llen('friends')).to.equal(1);
+      expect(redis.lrange('friends', 0, -1)).to.deep.equal(['Alice']);
     });
   });
 });
